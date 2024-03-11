23c544b6cbc4c6e70d83031f0d1941f2d7e7cd7e
diqing <diqing@qunhemail.com>
2024-03-11 15:34:09 +0800

    ...

--- lib/modules/main/main_controller.dart
+++ lib/modules/main/main_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/animation.dart';
-import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:get/get.dart';
 import 'package:ledger/modules/home/home_binding.dart';
 import 'package:ledger/modules/home/home_controller.dart';
@@ -15,24 +14,6 @@ import 'main_state.dart';
 
 class MainController extends GetxController {
   final MainState state = MainState();
-
-  @override
-  void onInit() {
-    FlutterBugly.init(
-      androidAppId: '8441e68dc5',
-      iOSAppId: 'e3ec751bd3',
-    );
-    FlutterBugly.setAppChannel('Android');
-    FlutterBugly.setUserId(StoreController.to.getUser()?.toString() ?? '');
-    FlutterBugly.putUserData(
-        key: 'user', value: StoreController.to.getUser()?.toString() ?? '');
-    super.onInit();
-  }
-
-  @override
-  void onClose() {
-    FlutterBugly.dispose();
-  }
 
   int getSelectIndex() => state.selectedIndex.value;
 
