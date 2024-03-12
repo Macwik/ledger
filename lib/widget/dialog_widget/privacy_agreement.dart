986baf1ea6777727a287e47b5615573ace4d5d0a
diqing <diqing@qunhemail.com>
2024-03-12 18:14:50 +0800

    ...

--- lib/widget/dialog_widget/privacy_agreement.dart
+++ lib/widget/dialog_widget/privacy_agreement.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ledger/res/colors.dart';
 
 class PrivacyAgreement extends StatelessWidget {
-  final formKey = GlobalKey<FormBuilderState>();
 
   @override
   Widget build(BuildContext context) {
