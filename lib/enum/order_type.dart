ede910898a9cd11c1dfbd076aaab58edb8a43d77
diqing <diqing@qunhemail.com>
2024-03-18 14:14:59 +0800

    ...

--- lib/enum/order_type.dart
+++ lib/enum/order_type.dart
@@ -5,6 +5,9 @@ enum OrderType {
   PURCHASE_RETURN,
   CREDIT,
   REPAYMENT,
+  INCOME,
+  COST,
+  REMITTANCE,
   ADD_STOCK,
   REFUND
 }
@@ -24,15 +27,20 @@ extension OrderTypeExtension on OrderType {
         return 4;
       case OrderType.CREDIT:
         return 5;
-      case OrderType.ADD_STOCK:
+      case OrderType.INCOME:
         return 6;
-      case OrderType.REFUND:
+      case OrderType.COST:
         return 7;
+      case OrderType.REMITTANCE:
+        return 8;
+      case OrderType.ADD_STOCK:
+        return 9;
+      case OrderType.REFUND:
+        return 10;
       default:
         throw Exception('Unsupported OrderType');
     }
   }
-
 
   String get desc {
     switch (this) {
@@ -47,11 +55,17 @@ extension OrderTypeExtension on OrderType {
       case OrderType.REPAYMENT:
         return '还款单';
       case OrderType.CREDIT:
-        return '欠款单';
+        return '录入欠款单';
+      case OrderType.INCOME:
+        return '收入单';
+      case OrderType.COST:
+        return '费用单';
+      case OrderType.REMITTANCE:
+        return '汇款单';
       case OrderType.ADD_STOCK:
-        return '直接入库';
+        return '直接入库单';
       case OrderType.REFUND:
-        return '退款';
+        return '退款单';
       default:
         throw Exception('Unsupported CustomDetailOrderType');
     }
