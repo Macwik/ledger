93740b52e60a0f10ad4bc4d8e93da125617edcc2
diqing <diqing@qunhemail.com>
2024-04-24 10:31:40 +0800

    ...

--- lib/modules/funds/cost_detail/cost_detail_state.dart
+++ lib/modules/funds/cost_detail/cost_detail_state.dart
@@ -1,18 +1,14 @@
 import 'package:ledger/entity/costIncome/cost_income_detail_dto.dart';
 
 class CostDetailState {
-
-  CostIncomeDetailDTO ? costIncomeDetailDTO;
-
-  //int ? id;
+  CostIncomeDetailDTO? costIncomeDetailDTO;
 
   CostDetailState() {
     ///Initialize variables
   }
+
 //上页面带过来的
- // CostIncomeOrderDTO? costIncomeOrderDTO;
   int? id;
 
   int? orderType;
-
 }
